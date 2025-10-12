89cad8bae0a50467c35345b1ad47968f885b54a3
asktoby <github@asktoby.com>
2025-10-12 14:51:16 +0100

    Changed font rendering to show emojis

--- VocabTesterCLI/Program.cs
+++ VocabTesterCLI/Program.cs
@@ -32,6 +32,7 @@ class Program
 
     static void Main()
     {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
         var rng = new Random();
         var remaining = Vocab.ToDictionary(v => v, v => QuizState.NeedEnglish);
 
